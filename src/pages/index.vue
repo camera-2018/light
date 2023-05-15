ce96acfe38a565733c9c543287ac85058422db19
camera-2018 <2907618001@qq.com>
2023-05-16 02:11:07 +0800

    feat: 落地

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,11 +4,46 @@
 
 <template>
   <TheHeader class="p-5" />
+  <div class="flex flex-row items-center justify-evenly gap-10 py-25">
+    <div class="flex flex-col items-start">
+      <h1 class="main_logo self-start justify-self-start bg-clip-text text-[3.6rem] font-bold text-dark-600 dark:text-light-600">
+        🐍🈵🈲⛰️队
+      </h1>
+      <h1 class="self-start justify-self-start bg-clip-text text-[3rem] font-bold dark:text-light-600">
+        恒生 “LIGHT” 金融科技杯
+      </h1>
+      <h1 class="self-start justify-self-start bg-clip-text text-[3rem] font-bold dark:text-light-600">
+        杭电第五届 Python 程序设计挑战赛
+      </h1>
+      <h1 class="from-pink-500 to-violet-500 bg-gradient-to-r bg-clip-text py-3 text-[5rem] font-bold text-transparent">
+        重剑无锋，大巧不工
+      </h1>
+      <div class="flex flex-row gap-5 py-3">
+        <button class="h-13 w-50 rounded-3xl bg-pink-600 text-lg text-light-100 transition duration-300 delay-150 ease-in-out hover:scale-110 hover:bg-indigo-500 hover:-translate-y-1">
+          查看我们的结果
+        </button>
+        <button class="h-13 w-50 rounded-3xl bg-orange-600 text-lg text-light-100 transition duration-300 delay-150 ease-in-out hover:scale-110 hover:bg-indigo-500 hover:-translate-y-1">
+          查看项目的 Github
+        </button>
+      </div>
+    </div>
+    <svg class="h-100 w-100 rounded-1 from-pink-500 to-violet-500 bg-gradient-to-r p-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><rect width="7.33" height="7.33" x="1" y="1" fill="#FFFFFF"><animate id="svgSpinnersBlocksWave0" attributeName="x" begin="0;svgSpinnersBlocksWave1.end+0.2s" dur="0.6s" values="1;4;1" /><animate attributeName="y" begin="0;svgSpinnersBlocksWave1.end+0.2s" dur="0.6s" values="1;4;1" /><animate attributeName="width" begin="0;svgSpinnersBlocksWave1.end+0.2s" dur="0.6s" values="7.33;1.33;7.33" /><animate attributeName="height" begin="0;svgSpinnersBlocksWave1.end+0.2s" dur="0.6s" values="7.33;1.33;7.33" /></rect><rect width="7.33" height="7.33" x="8.33" y="1" fill="#FFFFFF"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="8.33;11.33;8.33" /><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="1;4;1" /><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="7.33;1.33;7.33" /><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="7.33;1.33;7.33" /></rect><rect width="7.33" height="7.33" x="1" y="8.33" fill="#FFFFFF"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="1;4;1" /><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="8.33;11.33;8.33" /><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="7.33;1.33;7.33" /><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.1s" dur="0.6s" values="7.33;1.33;7.33" /></rect><rect width="7.33" height="7.33" x="15.66" y="1" fill="#FFFFFF"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="15.66;18.66;15.66" /><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="1;4;1" /><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33" /><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33" /></rect><rect width="7.33" height="7.33" x="8.33" y="8.33" fill="#FFFFFF"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="8.33;11.33;8.33" /><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="8.33;11.33;8.33" /><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33" /><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33" /></rect><rect width="7.33" height="7.33" x="1" y="15.66" fill="#FFFFFF"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="1;4;1" /><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="15.66;18.66;15.66" /><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33" /><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.2s" dur="0.6s" values="7.33;1.33;7.33" /></rect><rect width="7.33" height="7.33" x="15.66" y="8.33" fill="#FFFFFF"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="15.66;18.66;15.66" /><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="8.33;11.33;8.33" /><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="7.33;1.33;7.33" /><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="7.33;1.33;7.33" /></rect><rect width="7.33" height="7.33" x="8.33" y="15.66" fill="#FFFFFF"><animate attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="8.33;11.33;8.33" /><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="15.66;18.66;15.66" /><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="7.33;1.33;7.33" /><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.3s" dur="0.6s" values="7.33;1.33;7.33" /></rect><rect width="7.33" height="7.33" x="15.66" y="15.66" fill="#FFFFFF"><animate id="svgSpinnersBlocksWave1" attributeName="x" begin="svgSpinnersBlocksWave0.begin+0.4s" dur="0.6s" values="15.66;18.66;15.66" /><animate attributeName="y" begin="svgSpinnersBlocksWave0.begin+0.4s" dur="0.6s" values="15.66;18.66;15.66" /><animate attributeName="width" begin="svgSpinnersBlocksWave0.begin+0.4s" dur="0.6s" values="7.33;1.33;7.33" /><animate attributeName="height" begin="svgSpinnersBlocksWave0.begin+0.4s" dur="0.6s" values="7.33;1.33;7.33" /></rect></svg>
+  </div>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <h1 class="from-pink-500 to-violet-500 bg-gradient-to-r bg-clip-text py-3 text-[5rem] font-bold text-transparent">
+    收益概览
+  </h1>
   <div class="flex flex-col items-center justify-center">
-    <h1 class="from-pink-500 to-violet-500 bg-gradient-to-r bg-clip-text text-6xl font-bold text-transparent">
-      重剑无锋，大巧不工
-    </h1>
-    <br>
     <div class="flex flex-row items-center gap-2">
       <div class="justfy-center flex flex-row items-center gap-1">
         <input id="comments" name="comments" type="checkbox" class="h-4 w-4 rounded focus:border-4 focus:border-indigo-500/100">
